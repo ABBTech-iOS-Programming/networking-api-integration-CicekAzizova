8cbaef298395f98ef8a0eca8cc8cee0bd4b373bf
CicekAzizova <azizovacicak@gmail.com>
2026-08-23 01:24:14 +0400

    add MainTabView with tab navigation

---
+++ ProductShopApp/ProductShopApp/Features/MainTabView/MainTabView.swift
@@ -0,0 +1,56 @@
+//
+//  MainTabView.swift
+//  ProductShopApp
+//
+//  Created by Cicek on 21.08.26.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    
+    @State private var viewModel = ProductViewModel()
+    
+    var body: some View {
+        TabView {
+            
+            Tab {
+                NavigationStack {
+                    ProductListView(viewModel: viewModel)
+                }
+            } label: {
+                Image(.home)
+                    .renderingMode(.template)
+                    .foregroundStyle(.tabBar)
+                    
+            }
+            
+            Tab {
+                FavoriteView()
+            } label: {
+                Image(.heart)
+                    
+            }
+            
+            Tab {
+                
+            } label: {
+                Image(.square)
+                    
+            }
+            
+            Tab {
+                
+            } label: {
+                Image(.circle)
+                    
+            }
+
+        }
+        .tint(.badge)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
