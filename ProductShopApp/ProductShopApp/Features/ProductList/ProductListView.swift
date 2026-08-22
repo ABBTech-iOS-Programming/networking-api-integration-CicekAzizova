080f8e59f0a27822624fbd8d635c7fa3b3e95b87
CicekAzizova <azizovacicak@gmail.com>
2026-08-23 01:26:14 +0400

    add ProductListView

---
+++ ProductShopApp/ProductShopApp/Features/ProductList/ProductListView.swift
@@ -0,0 +1,118 @@
+//
+//  ProductListView.swift
+//  ProductShopApp
+//
+//  Created by Cicek on 21.08.26.
+//
+
+import SwiftUI
+
+struct ProductListView: View {
+    
+    @Bindable var viewModel: ProductViewModel
+    
+    @State  var selectedCategory: CategoryFilter = .all
+    
+    @State var searchText: String = ""
+    
+    var filteredProducts: [Product] {
+    let categoryFiltered = selectedCategory == .all
+        ? viewModel.products
+        : viewModel.products.filter { selectedCategory.matchingAPICategories.contains($0.category) }
+    
+    if searchText.isEmpty {
+        return categoryFiltered
+            } else {
+        return categoryFiltered.filter {
+            $0.title.contains(searchText) ||
+            ($0.brand?.contains(searchText) ?? false)
+            }
+        }
+    }
+  
+    private let columns: [GridItem] = [GridItem(.flexible()),GridItem(.flexible())]
+    
+    
+    var searchHolder: some View {
+        ZStack(alignment: .topLeading) {
+            HStack(alignment: .center) {
+                Image(systemName: "magnifyingglass")
+                    .resizable()
+                    .frame(width: 16, height: 16)
+                    .padding(.leading, 16)
+                TextField("Search products", text: $searchText)
+                    .font(.inter(.regular, size: 14))
+                    .padding(.leading, 20)
+                    
+                
+            }
+        }
+        .frame(height: 52)
+        .background(.white)
+        .clipShape(RoundedRectangle(cornerRadius: 16))
+        .padding(.bottom,16)
+    }
+    
+    var allCategory: some View {
+        LazyHStack {
+            ForEach(CategoryFilter.allCases){ category in
+                BadgeView(title: category.rawValue, horizontalPadding: 22, height: 38, cornerRadius: 19, weight: .inter(.semiBold, size: 12), background: .badge, foreground: .white)
+                    .onTapGesture {
+                        selectedCategory = category
+                    }
+            }
+        }
+    }
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color(.homeBackground)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    ScrollView {
+                        WelcomeBannerCardView(viewModel: viewModel)
+                            .padding(.bottom,14)
+                        searchHolder
+                        ScrollView(.horizontal) {
+                            allCategory
+                                .frame(height: 38)
+                            
+                        }
+                        .scrollIndicators(.hidden)
+                        
+                        LazyVGrid(columns: columns) {
+                            ForEach(filteredProducts){ product in
+                                NavigationLink(value: product) {
+                                    ProductCardView(product: product)
+                                }
+                                .buttonStyle(.plain)
+                               
+                            }
+                        }
+                        
+                        
+                    }
+                    
+                    .padding(.horizontal, 24)
+                }
+            }
+            
+            .navigationDestination(for: Product.self) { product in
+                            if let index = viewModel.products.firstIndex(where: { $0.id == product.id }) {
+                                ProductDetailsView(product: $viewModel.products[index])
+                            }
+                        }
+        }
+        .task {
+            await viewModel.fetchProduct()
+        }
+    }
+    
+        
+}
+
+#Preview {
+    ProductListView(viewModel: ProductViewModel(), selectedCategory: CategoryFilter.all)
+}
---
+++ ProductShopApp/ProductShopApp/Features/ProductList/WelcomeBannerCardView.swift
@@ -0,0 +1,67 @@
+//
+//  WelcomeBannerCardView.swift
+//  ProductShopApp
+//
+//  Created by Cicek on 21.08.26.
+//
+
+import SwiftUI
+
+struct WelcomeBannerCardView: View {
+    
+    let viewModel: ProductViewModel
+    
+    var discountText: String {
+        guard let discount = viewModel.products.first?.discountPercentage else {
+            return "0%"
+        }
+        return String(format: "%.0f%%", discount)
+    }
+    
+    var cardView: some View {
+        VStack(alignment: .leading,spacing: 7){
+            HStack {
+                greeting
+                Spacer()
+                BadgeView(title: discountText, horizontalPadding: 12, height: 28, cornerRadius: 14, weight: .inter(.semiBold, size: 12), background: .badge, foreground: .black)
+                    .padding(.trailing,20)
+
+            }
+            title
+            subtitle
+          
+            
+        }
+        .padding(.leading,18)
+        
+        .frame(height: 128)
+        .background(.welcomeCard)
+        .clipShape(RoundedRectangle(cornerRadius: 22))
+  
+    }
+    
+    var greeting: some View {
+        Text("Good morning")
+            .font(.inter(.regular, size: 12))
+            .foregroundStyle(.greeting)
+    }
+    
+    var title: some View {
+        Text("Find your next favorite product")
+            .font(.inter(.bold, size: 22))
+            .foregroundStyle(.white)
+    }
+    
+    var subtitle: some View {
+        Text("Fresh picks for you")
+            .font(.inter(.medium, size: 12))
+            .foregroundStyle(.subtitle)
+    }
+    var body: some View {
+        cardView
+    }
+}
+
+#Preview {
+    WelcomeBannerCardView(viewModel: ProductViewModel())
+}
